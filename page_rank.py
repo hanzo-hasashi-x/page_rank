f698e64158d5f37638c2b1d033273007b0f1a76a
hanzo-hasashi-x <iusan7756@gmail.com>
2025-12-12 10:11:41 +0100

    added comments and function description for linear page rank

--- page_rank.py
+++ page_rank.py
@@ -1,11 +1,16 @@
 import numpy as np
 
-def get_transform_matrix(W):
-    """ - the probability to move from node `i` to node `j`
+def get_transition_matrix(W):
+    """Returns transition matrix (P) based on graph weighted, adjacency matrix
+
+    `P[i][j]` - the probability to move from node `i` to node `j`
 
     Args:
         W (`np.matrix`):
             an adjacency matrix of a directed, weighted, regular graph G
+    Returns:
+        P (`np.array`):
+            transition matrix of `W`
     """
     P = np.zeros(W.shape)
     for i in range(W.shape[0]):
@@ -23,6 +28,19 @@ def get_transform_matrix(W):
 def pageRankLinear(A, alpha, v):
     """Returns the PageRank scores by solving the linear system equation
 
+    By solving equation of Markov chain: `x^T A = x^T`
+    Transition matrix on vector must remain the same.
+    That means that transition matrix became constant, stabilized-converged
+
+    By placing Google matrix in this equation, and developing it
+    `G = aP + (1-a)ev^T`
+
+    <=> x^T (aP+(1-a)ev^T) = x^T
+    <=> ax^TP + (1-a)x^Tev^T = x^T
+    <=> x^T - ax^TP = (1-a)v^T # because x^T * e = 1
+    <=> x^T(I - aP) = (1-a)v^T # because x^T * I = x^T | shapes: (1,n) x (n,n) = (1,n)
+    <=> (I - aP)^Tx = (1-a)v # just get transpose on both sides
+
     Args:
         A (`np.matrix`):
             an adjacency matrix of a directed, weighted, regular graph G
@@ -36,15 +54,14 @@ def pageRankLinear(A, alpha, v):
     """
 
     # get transform matrix
-    P = get_transform_matrix(A)
+    P = get_transition_matrix(A)
+    I = np.eye(A.shape[0])
 
-    # from Google matrix: G = aP + (1-a)ev.T
-
-    coeff_matrix = (np.eye(A.shape[0]) - alpha * P).T
+    coeff_matrix = (I - alpha * P).T
     b_vector = (1 - alpha) * v
 
+    # because of very deep math explanation, such equation has always solution
     scores = np.linalg.solve(coeff_matrix, b_vector)
-
     return scores
 
 def pageRankPower(A, alpha, v):
